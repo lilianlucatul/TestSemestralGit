46db415ab2c4177ba50502774727bcfdc45e9e88
lilianlucatul <116071566+lilianlucatul@users.noreply.github.com>
2023-03-29 15:43:25 +0200

    Feature/create user (#2)
    * Create user

---
+++ src/testsemestralgit/User.java
@@ -0,0 +1,13 @@
+package testsemestralgit;
+
+/**
+ *
+ * @author lilian.luca
+ */
+public class User {
+    private String name;
+    private String surname;
+    private int yearOfBirth;
+    
+    
+}
---
+++ src/testsemestralgit/User1.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package testsemestralgit;
+
+/**
+ *
+ * @author lilian.luca
+ */
+public class User1 {
+
+    private String name;
+    private String surname;
+    private int yearOfBirth;
+
+    public User1(String name, String surname, int yearOfBirth) {
+        this.name = name;
+        this.surname = surname;
+        this.yearOfBirth = yearOfBirth;
+    }
+
+    
+    
+}
